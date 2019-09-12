3814cc13a2d3e79c269bc2ab16e2d097701214ae
YungiKim1999 <danielkim1201@gmail.com>
2019-09-12 14:43:08 +1200

    Made play/pause button into a smart repeat button as well as adding in video slider.

--- src/main/java/wikispeak/UpgradedPlayScreenController.java
+++ src/main/java/wikispeak/UpgradedPlayScreenController.java
@@ -5,9 +5,7 @@ import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.ListView;
-import javafx.scene.control.SelectionMode;
-import javafx.scene.control.Slider;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
@@ -33,42 +31,71 @@ public class UpgradedPlayScreenController extends ListController {
     @FXML private BorderPane rootBorderPane;
     @FXML private ListView<String> creationList;
     @FXML private Slider volumeSlider;
+    @FXML private Label timeLabel;
+    @FXML private Label finishTime;
+    @FXML private Button playPauseButton;
+    @FXML private Slider videoBuffer;
 
     @FXML
     public void initialize(){
         firsTime = true;
         creationList.setItems(creationsStrings);
         creationList.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        timeLabel.setText("00:00");
+        finishTime.setText("00:00");
         creationList.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
             @Override
             public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
-                System.out.println(oldValue + " = old");
-                System.out.println(newValue + " = new");
-                if(oldValue == null){
                     getUserChoice(newValue);
                     setMediaForPlay();
                     playMedia();
+                    addVideoListener();
+                    addVolumeListener();
                     firsTime = false;
                     play = true;
-                }else{
-                    creationPlayingThing.stop();
-                    getUserChoice(newValue);
-                    setMediaForPlay();
-                    playMedia();
-                    play = true;
-                }
-
-
             }
         });
         volumeSlider.setValue(100);
+
+    }
+
+    private void addVolumeListener(){
         volumeSlider.valueProperty().addListener(observable -> {
-            if(play) {
                 creationPlayingThing.setVolume(volumeSlider.getValue() / 100);
-            }
         });
     }
 
+    private void setTimeLabels(Duration newVakue){
+        String currentTime = "";
+        currentTime += String.format("%02d", (int)newVakue.toMinutes());
+        currentTime += ":";
+        currentTime += String.format("%02d", (int)newVakue.toSeconds());
+        timeLabel.setText(currentTime);
+        String stopTime = "";
+        stopTime += String.format("%02d", (int)creationPlayingThing.getStopTime().toMinutes());
+        stopTime += ":";
+        stopTime += String.format("%02d", (int)creationPlayingThing.getStopTime().toSeconds());
+        finishTime.setText(stopTime);
+    }
+
+    private void addVideoListener() {
+        creationPlayingThing.currentTimeProperty().addListener(new ChangeListener<Duration>() {
+            @Override
+            public void changed(ObservableValue<? extends Duration> observable, Duration oldValue, Duration newValue) {
+                videoBuffer.setMin(0);
+                int endTime = Integer.parseInt(String.format("%02d", (int)creationPlayingThing.getStopTime().toMinutes())) * 60 +Integer.parseInt( String.format("%02d", (int)creationPlayingThing.getStopTime().toSeconds()));
+                int currentTime = Integer.parseInt(String.format("%02d", (int)newValue.toMinutes())) * 60 +Integer.parseInt( String.format("%02d", (int)newValue.toSeconds()));
+                videoBuffer.setMax(endTime);
+                videoBuffer.setValue(currentTime);
+                setTimeLabels(newValue);
+                if(timeLabel.getText().equals(finishTime.getText())){
+                    creationPlayingThing.stop();
+                    playPauseButton.setText("Repeat");
+                }
+
+            }
+        });
+    }
     @FXML
     private void handleExitButton()throws IOException {
         if(play == true){
@@ -79,6 +106,7 @@ public class UpgradedPlayScreenController extends ListController {
 
     private void playMedia(){
         creationPlayingThing.play();
+        playPauseButton.setText("Play/Pause");
     }
 
     private void pauseMedia(){
@@ -86,6 +114,9 @@ public class UpgradedPlayScreenController extends ListController {
     }
 
     private void setMediaForPlay(){
+        if (!firsTime && creationPlayingThing.getStatus() == MediaPlayer.Status.PLAYING){
+            creationPlayingThing.dispose();
+        }
         fileURL =new File("." + System.getProperty("file.separator") +"creations" + System.getProperty("file.separator") + selectedCreation + ".mp4");
         Media playCreation = new Media(fileURL.toURI().toString());
         creationPlayingThing = new MediaPlayer(playCreation);
@@ -100,19 +131,24 @@ public class UpgradedPlayScreenController extends ListController {
     @FXML
     private void handlePlayPauseButton(){
         if(!firsTime) {
-            if (creationPlayingThing.getStatus() == MediaPlayer.Status.PLAYING) {
-                System.out.println("Paused");
+            if(playPauseButton.getText().equals("Repeat")){
+                creationPlayingThing.stop();
+                selectedCreation = creationList.getSelectionModel().getSelectedItem();
+                setMediaForPlay();
+                playMedia();
+                addVideoListener();
+                addVolumeListener();
+                play = true;
+            } else if (creationPlayingThing.getStatus() == MediaPlayer.Status.PLAYING) {
                 pauseMedia();
                 play = false;
             } else if (creationPlayingThing.getStatus() == MediaPlayer.Status.PAUSED) {
-                System.out.println("Playing");
                 playMedia();
                 play = true;
+
             }
         }
     }
-
-
 
     @FXML
     private void handleForwardButton(){
